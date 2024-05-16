d272e8efc586faa9731a0df78ae236a72fe2167e
Vitor <vitor.espinozadg@gmail.com>
2024-05-16 18:34:45 -0300

    feat(WB-003): Creating auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,81 @@
+import { BadRequestException, Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "src/user/entity/user.entity";
+import { Repository } from "typeorm";
+import { UserDTO } from "src/user/dto/user.dto";
+import { UserService } from "src/user/user.service";
+import { Role } from "src/enums/role.enum";
+
+@Injectable()
+export class AuthService {
+private issuer = 'login';
+private audience = 'users';
+
+constructor(private readonly jwtService: JwtService, @InjectRepository(UserEntity) private usersRepository: Repository<UserEntity>, private userService: UserService) {}
+
+ createToken(user: UserEntity) {
+  return {
+    accessToken: this.jwtService.sign(
+      {
+      id: user.userId,
+      },
+      {
+        expiresIn: '2 days',
+        subject: String(user.userId),
+        issuer: this.issuer,
+        audience: this.audience
+      }
+    )
+  }
+}
+
+ checkToken(token: string) {
+  try {
+    const data = this.jwtService.verify(token, {
+        audience: this.audience,
+        issuer: this.issuer
+    })
+
+  return data;
+  } catch(e) {
+    throw new BadRequestException(e);
+  }
+
+}
+
+isValidToken(token: string) {
+  try {
+    this.checkToken(token);
+    return true;
+  }
+  catch(e) {
+    return false;
+  }
+}
+
+async login(email: string, password: string){
+    const user = await this.usersRepository.findOneBy({
+      email,
+      password
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('E-mail e/ou senha incorretos.');
+    }
+
+    return this.createToken(user);
+ 
+}
+
+
+async register(data: UserDTO, isAdmin = false)
+{
+    isAdmin ? data.role = Role.ADMIN : data.role = Role.ASSOCIATE;
+  
+    const user = await this.userService.create(data);
+
+    return this.createToken(user);
+}
+
+}
