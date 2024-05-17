5141383af1dab1a9c92145679d3ad52905006307
Vitor <vitor.espinozadg@gmail.com>
2024-05-17 15:18:22 -0300

    feat: Creating param id decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Post, Put } from "@nestjs/common";
 import { UserDTO } from "./dto/user.dto";
 import { UserService } from "./user.service";
+import { ParamId } from "src/decorators/param-id.decorator";
 
 @Controller('users')
 export class UserController {
@@ -17,17 +18,17 @@ export class UserController {
     }
 
     @Get(':id') 
-    async readById(@Param('id', ParseIntPipe) id) {
+    async readById(@ParamId() id: number) {
         return this.userService.readById(id);
     }
 
     @Put(':id') 
-    async update(@Body() body : UserDTO, @Param('id', ParseIntPipe) id) {
+    async update(@Body() body : UserDTO, @ParamId() id: number) {
         return this.userService.update(id, body);
     }
 
     @Delete(':id')
-    async delete(@Param('id', ParseIntPipe) id) {
+    async delete(@ParamId() id: number) {
         return this.userService.delete(id);
     }
     
