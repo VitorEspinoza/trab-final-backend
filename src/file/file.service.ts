22e35d212b23caa433f190d1766baad6397476ed
Vitor <vitor.espinozadg@gmail.com>
2024-05-29 23:18:52 -0300

    feat(WB-011): Create file module

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from "@nestjs/common";
-import { writeFile } from "fs/promises";
-
+import { v4 as uuidv4 } from 'uuid';
+import { BlobServiceClient } from "@azure/storage-blob";
+import { Readable } from "stream";
 @Injectable()
 export class FileService {
 
-async upload(file: Express.Multer.File, path: string) {
-        return writeFile(path, file.buffer);
+async uploadUserPhoto(photo: Express.Multer.File, fileName: string = uuidv4().toString() + photo.mimetype.replace('image/', '.')) {
+        const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+        const containerClient = blobServiceClient.getContainerClient('user-photos');
+        await containerClient.setAccessPolicy('blob');
+        const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+        const fileStream = Readable.from(photo.buffer);
+        return {
+           uploadResponse: await blockBlobClient.uploadStream(fileStream),
+           fileName: fileName
+        }
     }
 }
+
