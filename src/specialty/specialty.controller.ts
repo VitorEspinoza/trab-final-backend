ab40fa24f8624cc275669255d8a28b5674464e3d
Vitor <vitor.espinozadg@gmail.com>
2024-06-05 15:28:45 -0300

    feat(WB-003): adding authorization to the specialty apis

--- src/specialty/specialty.controller.ts
+++ src/specialty/specialty.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from "@nestjs/common";
 import { SpecialtyService } from "./specialty.service";
 import { SpecialtyDTO } from "./dto/specialty.dto";
 
+import { Roles } from "src/decorators/role.decorator";
+import { AuthGuard } from "src/guards/auth.guard";
+import { RoleGuard } from "src/guards/role.guard";
+import { Role } from "src/enums/role.enum";
+
+@Roles(Role.ADMIN)
+@UseGuards(AuthGuard, RoleGuard)
 @Controller('specialties')
 export class SpecialtyController {
 
--- src/specialty/specialty.module.ts
+++ src/specialty/specialty.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { SpecialtyService } from "./specialty.service";
 import { SpecialtyController } from "./specialty.controller";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthModule } from "src/auth/auth.module";
+import { UserModule } from "src/user/user.module";
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, AuthModule, UserModule],
     controllers: [SpecialtyController],
     providers: [SpecialtyService]
 })
