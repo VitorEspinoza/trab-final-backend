3c9e2c5f487f2ed8d1bf2aea65b87afe07571426
Vitor <vitor.espinozadg@gmail.com>
2024-06-05 20:20:54 -0300

    refact: changing the import method in the doctor module

--- src/doctor/doctor.module.ts
+++ src/doctor/doctor.module.ts
@@ -1,15 +1,16 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 import { PrismaModule } from 'src/prisma/prisma.module';
 
-import { UserModule } from 'src/user/user.module';
+
 import { DoctorController } from './doctor.controller';
 import { DoctorService } from './doctor.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserService } from 'src/user/user.service';
 
 @Module({
-  imports: [PrismaModule, UserModule, forwardRef(() => AuthModule)],
+  imports: [PrismaModule, AuthModule],
   controllers: [DoctorController],
-  providers: [DoctorService],
+  providers: [DoctorService, UserService],
 })
 export class DoctorModule {}
