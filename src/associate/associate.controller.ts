7850a0a868ffc2ae16aade3b1fec96633ec604af
felipebassetti <bassettifelipe@gmail.com>
2024-06-06 21:53:44 -0300

    refactor(FEAT/WB-006): refactor associate's CRUD

--- src/associate/associate.controller.ts
+++ src/associate/associate.controller.ts
@@ -1,16 +1,43 @@
-import { Controller, Post, Body } from "@nestjs/common";
-import { AssociateService } from "./associate.service";
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Delete,
+  Put,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { AssociateService } from './associate.service';
 
-import { CreateAssociateDto } from "./dto/create-associate.dto";
-
+import { CreateAssociateDto } from './dto/create-associate.dto';
+import { AssociateDto } from './dto/associate.dto';
 
 @Controller('associates')
 export class AssociateController {
-    constructor(private readonly associateService: AssociateService) {}
+  constructor(private readonly associateService: AssociateService) {}
 
-    @Post()
-    async create(@Body() body: CreateAssociateDto) {
-        return this.associateService.create(body);
-    }
+  @Post()
+  async create(@Body() body: CreateAssociateDto) {
+    return this.associateService.create(body);
+  }
+  @Get()
+  async read() {
+    return this.associateService.read();
+  }
 
+  @Get(':id')
+  async readById(@Param('id') id: string) {
+    return this.associateService.readById(id);
+  }
+
+  @Put(':id')
+  async update(@Body() body: AssociateDto, @Param('id') id: string) {
+    return this.associateService.update(id, body);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.associateService.delete(id);
+  }
 }
--- src/associate/associate.service.ts
+++ src/associate/associate.service.ts
@@ -1,65 +1,105 @@
 import { UserService } from 'src/user/user.service';
-import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
-import { PrismaService } from "src/prisma/prisma.service";
-import { CreateAssociateDto } from "./dto/create-associate.dto";
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAssociateDto } from './dto/create-associate.dto';
 import { AuthService } from 'src/auth/auth.service';
 import { Role } from 'src/enums/role.enum';
 
-
 @Injectable()
-export class AssociateService{
-    constructor(
-      private prismaService: PrismaService,
-      private UserService: UserService,
-      private authService: AuthService
-) {}
+export class AssociateService {
+  constructor(
+    private prismaService: PrismaService,
+    private UserService: UserService,
+    private authService: AuthService,
+  ) {}
 
-    async create(data: CreateAssociateDto) {
-        await this.UserService.verifyEmailExists(data.user.email);
+  async create(data: CreateAssociateDto) {
+    await this.UserService.verifyEmailExists(data.user.email);
 
-        const existingAssociate = await this.prismaService.associate.findFirst({
-            where: {
-                document: data.document,
-            }
-        })
+    const existingAssociate = await this.prismaService.associate.findFirst({
+      where: {
+        document: data.document,
+      },
+    });
 
-        if(existingAssociate) {
-            throw new BadRequestException('Este associado já está vinculado ao plano');
-        }
-    
-        data.user.role = Role.ASSOCIATE;
-        const associate = 
-            {...data, 
-                user: { create: data.user},
-                address: {
-                    connectOrCreate: {
-                        where: {
-                            zipCode_number: {
-                                zipCode: data.address.zipCode,
-                                number: data.address.number
-                            }
-                        },
-                        create: data.address
-                            
-                    }
-                },       
-                healthInsuranceIdentifier: this.generateInsuranceIdentifierString()
-            } 
-
-        const savedAssociate = await this.prismaService.associate.create({ data: associate} );
-
-        return this.authService.createToken(savedAssociate.userId);
+    if (existingAssociate) {
+      throw new BadRequestException(
+        'Este associado já está vinculado ao plano',
+      );
     }
 
-    generateInsuranceIdentifierString() {
-        let result = '';
-        while(result.length < 20){
-            result += Math.floor(Math.random() * 10); 
-        }
-        return result;
+    data.user.role = Role.ASSOCIATE;
+    const associate = {
+      ...data,
+      user: { create: data.user },
+      address: {
+        connectOrCreate: {
+          where: {
+            zipCode_number: {
+              zipCode: data.address.zipCode,
+              number: data.address.number,
+            },
+          },
+          create: data.address,
+        },
+      },
+      healthInsuranceIdentifier: this.generateInsuranceIdentifierString(),
+    };
+
+    const savedAssociate = await this.prismaService.associate.create({
+      data: associate,
+    });
+
+    return this.authService.createToken(savedAssociate.userId);
+  }
+
+  generateInsuranceIdentifierString() {
+    let result = '';
+    while (result.length < 20) {
+      result += Math.floor(Math.random() * 10);
     }
+    return result;
+  }
 
+  async read() {
+    return this.prismaService.associate.findMany();
+  }
 
+  async readById(id: string) {
+    await this.exists(id);
 
+    return this.prismaService.associate.findUnique({
+      where: {
+        associateId: id,
+      },
+    });
+  }
+
+  async update(id: string, data: CreateAssociateDto) {
+    await this.exists(id);
+  }
+
+  async delete(id: string) {
+    await this.exists(id);
+    return this.prismaService.associate.delete({
+      where: { associateId: id },
+    });
+  }
+
+  async exists(id: string) {
+    const count = await this.prismaService.associate.count({
+      where: {
+        associateId: id,
+      },
+    });
+
+    if (count === 0) {
+      throw new NotFoundException(`O associado com o id ${id} não existe.`);
+    }
+  }
 }
