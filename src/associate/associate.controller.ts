6c81f43ae4fd289423488a40aab7e4f023170f3c
Vitor <vitor.espinozadg@gmail.com>
2024-06-07 17:42:10 -0300

    feat(WB-003): adding authorization to the associate apis

--- src/associate/associate.controller.ts
+++ src/associate/associate.controller.ts
@@ -7,12 +7,20 @@ import {
   Delete,
   Put,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { AssociateService } from './associate.service';
 
 import { CreateAssociateDto } from './dto/create-associate.dto';
 import { AssociateDto } from './dto/associate.dto';
 
+import { Roles } from 'src/decorators/role.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { RoleGuard } from 'src/guards/role.guard';
+import { Role } from 'src/enums/role.enum';
+
+@Roles(Role.ADMIN, Role.ASSOCIATE)
+@UseGuards(AuthGuard, RoleGuard)
 @Controller('associates')
 export class AssociateController {
   constructor(private readonly associateService: AssociateService) {}
@@ -21,6 +29,8 @@ export class AssociateController {
   async create(@Body() body: CreateAssociateDto) {
     return this.associateService.create(body);
   }
+    
+  @Roles(Role.ADMIN)
   @Get()
   async read() {
     return this.associateService.read();
