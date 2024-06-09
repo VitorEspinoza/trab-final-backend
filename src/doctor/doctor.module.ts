878e62eea8253be4e4ccb55380e5127d94d6a7d5
Vitor <vitor.espinozadg@gmail.com>
2024-06-09 12:17:18 -0300

    feat(WB-011): Implementing a photo deletion method

--- src/doctor/doctor.module.ts
+++ src/doctor/doctor.module.ts
@@ -8,10 +8,11 @@ import { DoctorService } from './doctor.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserService } from 'src/user/user.service';
 import { UnitModule } from 'src/unit/unit.module';
+import { FileService } from 'src/file/file.service';
 
 @Module({
   imports: [PrismaModule, AuthModule, UnitModule],
   controllers: [DoctorController],
-  providers: [DoctorService, UserService],
+  providers: [DoctorService, FileService, UserService],
 })
 export class DoctorModule {}
--- src/unit/unit.module.ts
+++ src/unit/unit.module.ts
@@ -5,11 +5,12 @@ import { UnitService } from './unit.service';
 import { UnitController } from './unit.controller';
 import { AddressModule } from 'src/Address/address.module';
 import { UserService } from 'src/user/user.service';
+import { FileService } from 'src/file/file.service';
 
 @Module({
   imports: [PrismaModule, AuthModule, AddressModule],
   controllers: [UnitController],
-  providers: [UnitService, UserService],
+  providers: [UnitService, FileService, UserService],
   exports: [UnitService]
 })
 export class UnitModule {}
